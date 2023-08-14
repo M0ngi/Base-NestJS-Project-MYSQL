24a0330270a741afd72bfdb74324baecd299c80a
Mohamed Mongi Saidane <saidanemongi@gmail.com>
2023-08-14 17:25:33 +0100

    Init

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@config/config.module';
+import * as configs from '@config/configs/index';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@config/config.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        '.env.local',
+        '.env',
+        '.env.development.local',
+        '.env.development',
+      ],
+      load: Object.keys(configs).map((key) => configs[key]),
+      expandVariables: true,
+      cache: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return configService.getTypeormConfig();
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
